4d81aeedc34c966dc5b868f4ad6640a029dac33c
ncoovr <nicolasvelascoreyes@gmail.com>
2026-06-01 22:12:18 -0500

    añadi una base de datos falsa momentanea para probar en mockData.js, ademas empece a editar el feed de pinterest con las imagenes de la bdd

---
+++ Frontend-Pinterest/assets/js/mockData.js
@@ -0,0 +1,28 @@
+// Esta es nuestra base de datos simulada
+const mockPins = [
+    { 
+        id: 1, 
+        title: "Besucon", 
+        imageUrl: "https://images.pexels.com/photos/9329047/pexels-photo-9329047.jpeg?auto=compress&cs=tinysrgb&w=600" 
+    },
+    { 
+        id: 2, 
+        title: "Tigre", 
+        imageUrl: "https://images.pexels.com/photos/9221307/pexels-photo-9221307.jpeg?auto=compress&cs=tinysrgb&w=600" 
+    },
+    { 
+        id: 3, 
+        title: "Cachorra", 
+        imageUrl: "https://images.pexels.com/photos/1666667/pexels-photo-1666667.jpeg?auto=compress&cs=tinysrgb&w=600" 
+    },
+    { 
+        id: 4, 
+        title: "Chonerito", 
+        imageUrl: "https://images.pexels.com/photos/36770103/pexels-photo-36770103.jpeg?auto=compress&cs=tinysrgb&w=600" 
+    },
+    { 
+        id: 5, 
+        title: "Flaquito", 
+        imageUrl: "https://images.pexels.com/photos/30150825/pexels-photo-30150825.jpeg?auto=compress&cs=tinysrgb&w=600" 
+    }
+];
