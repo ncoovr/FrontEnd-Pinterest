80dd0968f4fc806fe891a3b542c4880f65783d4e
ncoovr <nicolasvelascoreyes@gmail.com>
2026-06-12 16:18:14 -0500

    añadi la galleria momentanea antes de S3

---
+++ Frontend-Pinterest/src/components/organisms/GalleryGrid/GalleryGrid.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./GalleryGrid.scss";
+
+export default function GalleryGrid() {
+    const imagenes = [
+        "https://images.pexels.com/photos/9329047/pexels-photo-9329047.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/9221307/pexels-photo-9221307.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/1666667/pexels-photo-1666667.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/36770103/pexels-photo-36770103.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/30150825/pexels-photo-30150825.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/35719467/pexels-photo-35719467.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/15988007/pexels-photo-15988007.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/12379827/pexels-photo-12379827.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/3724836/pexels-photo-3724836.jpeg?auto=compress&cs=tinysrgb&w=600"
+    ];
+
+    return (
+        <section className="cuadricula-galeria">
+            {imagenes.map((imgUrl, index) => (
+                <img key={index} src={imgUrl} alt={`Contenido de galería ${index + 1}`} />
+            ))}
+        </section>
+    );
+}
--- Frontend-Pinterest/src/pages/Index/Index.jsx
+++ Frontend-Pinterest/src/pages/Index/Index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../../components/organisms/Header/Header";
 import Hero from "../../components/organisms/Hero/Hero";
 import CategoryBar from "../../components/organisms/CategoryBar/CategoryBar";
+import GalleryGrid from "../../components/organisms/GalleryGrid/GalleryGrid";
+import "./Index.scss";
 
 export default function Index() {
     return (
@@ -10,7 +12,7 @@ export default function Index() {
             <main className="contenedor-feed">
                 <Hero />
                 <CategoryBar />
-                {}
+                <GalleryGrid />
             </main>
         </div>
     );
