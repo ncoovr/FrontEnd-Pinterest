5289395943552e4bfacb981ffe8036cf4d73ca4e
ncoovr <nicolasvelascoreyes@gmail.com>
2026-06-12 16:09:05 -0500

    integree todo en el index

--- Frontend-Pinterest/src/pages/Index/Index.jsx
+++ Frontend-Pinterest/src/pages/Index/Index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../../components/organisms/Header/Header";
-import "./Index.scss"; 
+import Hero from "../../components/organisms/Hero/Hero";
 
 export default function Index() {
     return (
         <div className="pagina-principal">
             <Header />
             <main className="contenedor-feed">
-                {}
+                <Hero />
             </main>
         </div>
     );
